b42361d75b34223b352999369e9a105f72ce4367
selmo97 <selena.moss2@gmail.com>
2025-06-17 15:18:39 -0700

    ShowSelection component

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,10 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({shows, setSelectedShow}) {
+  return <nav className="shows">
+    {shows.map(show=><a className="show"
+      key = {show.name}
+      onClick={()=>setSelectedShow(show)}>{show.name}</a>)}
+  </nav>;
 }
