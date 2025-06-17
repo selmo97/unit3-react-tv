56cc0214bfb486ee3258cf62c844ff9c0d0812ee
selmo97 <selena.moss2@gmail.com>
2025-06-17 00:35:50 -0700

    ShowDetails component

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,21 @@
 import "./shows.css";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
+import { useState } from "react";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  const [selectedEps, setSelectedEps] = useState()
+  if(!show){
+    return(
+      <p>Please select a show to view more details</p>
+    )
+  }
+
+  return <div className="show-details">
+    <EpisodeList name={show.name} episodes={show.episodes} selectedEpisode={selectedEps} setSelectedEpisode={setSelectedEps} />
+    <EpisodeDetails episode= {selectedEps} />
+    
+
+  </div>;
 }
